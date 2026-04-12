db4ff20cf4388d7c4961404dd8c77a5517078f72
DW1804 <23521804@gm.uit.edu.vn>
2026-04-12 21:33:40 +0700

    Initial commit

---
+++ main.py
@@ -0,0 +1,16 @@
+﻿from fastapi import FastAPI
+from pydantic import BaseModel
+import random
+
+app = FastAPI()
+class PredictInput(BaseModel):
+    feature_1: float
+    feature_2: float
+@app.post("/api/predict")
+async def predict_status(data: PredictInput):
+    """
+    Nhận JSON input, xử lý mock logic và trả về kết quả
+    """
+    mock_result = random.choice(["attack", "normal"])
+   
+    return {"status": mock_result}
